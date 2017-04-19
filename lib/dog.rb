13e58c4cfc7865afba7b6eac9c1ca2c8e482bbbc
lydie  toussaint <lydietoussaint123@gmail.com>
2017-04-19 14:59:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+
+  def initi(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_first)
+    @name = new_first
+  end
+
+  def breed(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+end
